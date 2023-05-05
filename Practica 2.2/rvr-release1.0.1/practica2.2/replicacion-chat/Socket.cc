701326ed82c5bdbfe84cd659cbef6742113179cd
Sol Flora López Antón <solflora@ucm.es>
2023-05-05 02:19:59 +0200

    Ejercicio4

--- Practica 2.2/rvr-release1.0.1/practica2.2/replicacion-chat/Socket.cc
+++ Practica 2.2/rvr-release1.0.1/practica2.2/replicacion-chat/Socket.cc
@@ -6,7 +6,30 @@
 Socket::Socket(const char * address, const char * port):sd(-1)
 {
     //Construir un socket de tipo AF_INET y SOCK_DGRAM usando getaddrinfo.
+    struct addrinfo hints;
+    struct addrinfo *result;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+
+    hints.ai_flags = AI_PASSIVE; //Devolver 0.0.0.0
+    hints.ai_family = AF_INET; //IPv4
+    hints.ai_socktype = SOCK_DGRAM; //UDP
+
+    int rc = getaddrinfo(address, port, &hints, &result);
+
+    if (rc != 0) {
+        std::cerr << "getaddrinfo: " << gai_strerror(rc) << std::endl;
+    }
+
     //Con el resultado inicializar los miembros sd, sa y sa_len de la clase
+    int sd = socket(result->ai_family, result->ai_socktype, 0);
+
+    if(sd == -1){
+        std::cerr << "Failed to create socket" << std::endl;
+    }
+
+    sa = *result->ai_addr;
+	sa_len = result->ai_addrlen;
 }
 
 int Socket::recv(Serializable &obj, Socket * &sock)
@@ -36,14 +59,23 @@ int Socket::recv(Serializable &obj, Socket * &sock)
 int Socket::send(Serializable& obj, const Socket& sock)
 {
     //Serializar el objeto
+    obj.to_bin();
     //Enviar el objeto binario a sock usando el socket sd
+    int rc = sendto(sd, obj.data, obj.size, 0, &sock.sa, sock.sa_len);
+    if(rc <= 0) return -1;
+    return 0;
 }
 
 bool operator== (const Socket &s1, const Socket &s2)
 {
     //Comparar los campos sin_family, sin_addr.s_addr y sin_port
     //de la estructura sockaddr_in de los Sockets s1 y s2
+    sockaddr_in *in1 = (sockaddr_in*) &s1.sa;
+    sockaddr_in *in2 = (sockaddr_in*) &s2.sa;
+
     //Retornar false si alguno difiere
+    return (in1->sin_family == in2->sin_family && in1->sin_addr.s_addr == in2->sin_addr.s_addr 
+        && in1->sin_port == in2->sin_port);
 };
 
 std::ostream& operator<<(std::ostream& os, const Socket& s)
